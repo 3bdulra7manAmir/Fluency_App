74ba28b34adab3f434e0c47274fa6f7228f34f48
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-31 15:24:56 +0200

    login sector nearly there

--- lib/features/login/widgets/login_sector.dart
+++ lib/features/login/widgets/login_sector.dart
@@ -1,4 +1,7 @@
+import 'package:fluency/core/utils/constants.dart';
+import 'package:fluency/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginColumn extends StatelessWidget
 {
@@ -7,11 +10,38 @@ class LoginColumn extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return Stack(
-      children:
-      [
-        
-      ],
+    return Center(
+      child: Stack(
+        children:
+        [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:
+            [
+              Container(
+                width: KMediaQuery(context).width * 0.87,
+                height: 330.h,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(24.r)
+                ),
+                child: Column(
+                  children:
+                  [
+                    const SizedBox(height: 24,),
+
+                    Text('Welcome back!', style: Styles.textStyle24,),
+
+                    const SizedBox(height: 24,),
+
+                    
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
