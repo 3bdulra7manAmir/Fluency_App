1d8cd22e1b2e1bf797ea538542a5f72b12ed7ba9
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-31 15:24:04 +0200

    using custom font

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -5,26 +5,43 @@ abstract class Styles
 {
   static TextStyle textStyle30 = TextStyle(
     fontSize: 30.sp,
-    fontWeight: FontWeight.w900,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Nunito',
   );
 
   static TextStyle textStyle20 = TextStyle(
     fontSize: 20.sp,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Nunito',
+  );
+
+  static TextStyle textStyle24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Nunito-Bold',
   );
 
   static TextStyle textStyle18 = TextStyle(
     fontSize: 18.sp,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Nunito',
   );
 
   static TextStyle textStyle16 = TextStyle(
     fontSize: 16.sp,
-    fontWeight: FontWeight.w500,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Nunito',
   );
 
   static TextStyle textStyle14 = TextStyle(
     fontSize: 14.sp,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Nunito',
   );
 }
