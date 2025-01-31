180d7bd2167b4981584b61026c5334514a95f3d1
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-31 15:25:19 +0200

    purple sector only border part remains

--- lib/features/login/widgets/purple_sector.dart
+++ lib/features/login/widgets/purple_sector.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: unused_import
+
+import 'package:fluency/core/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FirstPurpleStack extends StatelessWidget
 {
@@ -11,6 +16,13 @@ class FirstPurpleStack extends StatelessWidget
       children:
       [
         //Positioned or Positioned.fill
+        Container(
+          decoration: BoxDecoration(
+            color: AppColors.kSecondGradient,
+            image: DecorationImage(image: AssetImage('assets/images/png/Login_View_Bg.png'), fit: BoxFit.cover),
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40.r), bottomRight: Radius.circular(40.r)),
+          ),
+        ),
       ],
     );
   }
