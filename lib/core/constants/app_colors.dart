925137c7c5f8da7992185a60237d45610c52f5fd
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-02-10 07:31:53 +0200

    all app colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 class AppColors
 {
-  static const Color kFirstGradient = Color(0xff662D91);
-  static const Color kSecondGradient = Color(0xff905EB6);
+  static const Color kFirstGradientColor = Color(0xff662D91);
+  static const Color kSecondGradientColor = Color(0xff905EB6);
 
-  static const Color kAlmostWhite = Color(0xfff3f5fc);
+  static const Color kAlmostWhiteColor = Color(0xfff3f5fc);
 
-  static const Color kForgetPassword = Color(0xff4F1993);
-  static const Color kDontHaveAccount = Color(0xff777777);
+  static const Color kForgetPasswordColor = Color(0xff4F1993);
+  static const Color kDontHaveAccountColor = Color(0xff777777);
 
-  static const Color kScaffoldBG = Color(0xfff7f6fc);
-  static const Color kText = Color(0xff362E3B);
-
+  static const Color kScaffoldBGColor = Color(0xfff7f6fc);
+  static const Color kTitleTextColor = Color(0xff362E3B);
+  
+  static const Color kTextInsideFormFieldColor = Color(0xff777777);
+  static const Color kTextFormFieldFillColor = Color(0xffFCFCFC);
+  static const Color kTextFormFieldBorderSideColor = Color(0xffF0F0F0);
+  
+  static const Color kPurpleButtonColor = Colors.transparent;
+  static const Color kLoginFormBoxDecorationColor = Colors.white;
 }
