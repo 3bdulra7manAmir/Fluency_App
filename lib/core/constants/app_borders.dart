ffb1cc565b4996bca238a2162c5c816ace6575ee
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-02-10 07:31:45 +0200

    all app borders

---
+++ lib/core/constants/app_borders.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppBorders
+{
+  BorderRadius radiusCircular24 = BorderRadius.circular(24.r);
+  BorderRadius radiusCircular25 = BorderRadius.circular(25.r);
+}
