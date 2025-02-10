6806e3dafe4d4e4e260dc42d725570e84b75e6fc
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-02-10 11:26:05 +0200

    constants update

--- lib/core/constants/app_borders.dart
+++ lib/core/constants/app_borders.dart
@@ -4,5 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class AppBorders
 {
   BorderRadius radiusCircular24 = BorderRadius.circular(24.r);
-  BorderRadius radiusCircular25 = BorderRadius.circular(25.r);
+  BorderRadius radiusCircular80 = BorderRadius.circular(80.r);
+  BorderRadius radiusCircular50 = BorderRadius.circular(50.r);
 }
