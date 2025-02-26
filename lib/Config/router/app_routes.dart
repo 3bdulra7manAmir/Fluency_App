84f95b10b85e812bd09ae1e52fd98c1e3b6a2365
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-02-26 20:50:09 +0200

    new AppRoutes.dart

---
+++ lib/Config/router/app_routes.dart
@@ -0,0 +1,12 @@
+abstract class AppRoutes
+{
+  AppRoutes._();
+
+  static const kSplashView = '/SplashView';
+  static const kLoginView = '/LoginView';
+  static const kNotificationsView = '/NotificationsView';
+  static const kNoNotificationsView = '/NoNotificationsView';
+  static const kTeachersView = '/TeachersView';
+  static const kTeachersAPIView = '/TeachersAPIView';
+  
+}
